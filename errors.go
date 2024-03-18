c1272e1e27762a6a756c20b398ef54e59c917683
daemotron <mail@daemotron.net>
2024-03-18 23:42:03 +0100

    add documentation to errors

--- errors.go
+++ errors.go
@@ -3,5 +3,6 @@ package appdirs
 import "errors"
 
 var (
+	// ErrAllUsersProfileNotDefined %ALLUSERSPROFILE% environment not defined (Windows only)
 	ErrAllUsersProfileNotDefined = errors.New("ALLUSERSPROFILE environment not defined")
 )
