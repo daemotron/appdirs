90360240979396362a17572ae831549cefa21966
daemotron <mail@daemotron.net>
2024-03-21 11:36:17 +0100

    centralize common elements

---
+++ dirs_common.go
@@ -0,0 +1,11 @@
+package appdirs
+
+import "os/user"
+
+func getHomeDir() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return usr.HomeDir, nil
+}
--- dirs_unix.go
+++ dirs_unix.go
@@ -5,18 +5,9 @@ package appdirs
 import (
 	"fmt"
 	"os"
-	"os/user"
 	"path/filepath"
 	"strings"
 )
-
-func getHomeDir() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	return usr.HomeDir, nil
-}
 
 func (conf AppConf) userDataDir() (string, error) {
 	var base string
